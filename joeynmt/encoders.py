eb6c71f376b567f76e0cdd583a5428c0688ed425
czarnetzki <czarnetzki@cl.uni-heidelberg.de>
2020-02-12 18:28:43 +0100

    commented out prints and last linear layer in encoder

--- joeynmt/encoders.py
+++ joeynmt/encoders.py
@@ -291,13 +291,13 @@ class SpeechRecurrentEncoder(Encoder):
         # apply dropout to the rnn input
         conv_do = self.rnn_input_dropout(conv_out2)
 
-        print(conv_do.size())
-        print("convolution length", conv_length)
+        #print(conv_do.size())
+        #print("convolution length", conv_length)
         packed = pack_padded_sequence(conv_do, conv_length, batch_first=True)
         #print("packed: ", packed)
         output, hidden = self.rnn(packed)
 
-        print("output", output[0].shape)
+        #print("output", output[0].shape)
 
         #pylint: disable=unused-variable
         if isinstance(hidden, tuple):
@@ -328,10 +328,10 @@ class SpeechRecurrentEncoder(Encoder):
             output = self.norm_out(output)
 
         # a non-linear activation for the output layer
-        if self.last_activation == "relu":
-            output = torch.relu(output)
-        elif self.last_activation == "tanh":
-            output = torch.tanh(output)
+        #if self.last_activation == "relu":
+         #   output = torch.relu(output)
+        #elif self.last_activation == "tanh":
+        #    output = torch.tanh(output)
 
         # final: batch x directions*hidden
         return output, hidden_concat
